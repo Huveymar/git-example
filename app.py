8f773e95e5670ebec6b791f195963b871c2864c5
wilson huveymar muñoz <wilsonmunoz2020@itp.edu.co>
2020-11-24 15:54:04 -0500

    applicacion de estilos

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
 app = Flask(__name__)
+db = sqlite3.connect('data.db', check_same_thread=False)
+
 # rutas
 @app.route('/') # / significa la ruta raiz
 def index():
@@ -27,5 +31,35 @@ def contacto():
 def sumar():
     resultado = 2+2
     return 'La suma de 2+2 =' + str(resultado)
+
+@app.route('/usuarios') # nombre ruta
+def usuarios():
+    usuarios = db.execute('select * from usuarios')
+    
+    usuarios = usuarios.fetchall()
+    return render_template('usuarios/listar.html', usuarios=usuarios )
+
+@app.route('/usuarios/crear', methods=['GET','POST']) # nombre ruta
+def crear_usuarios():
+    if request.method == 'GET':
+        return render_template('usuarios/crear.html')
+   
+    nombres = request.form.get('nombres')
+    apellidos = request.form.get('apellidos')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    cursor = db.cursor()
+    cursor.execute("""insert into usuarios(
+        nombres,
+        apellidos,
+        email,
+        password
+    )values (?,?,?,?)
+    """, (nombres, apellidos, email, password))
+
+    db.commit()
+
+    return redirect(url_for('usuarios'))
 
 app.run(debug=True, port=5100)
